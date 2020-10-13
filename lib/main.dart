2eae541d3f91e3101833ff1ec6373d3279733ceb
Vishwas verma <vermavarun932@gmail.com>
2020-10-13 13:02:35 +0530

    statefulwifget

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,52 @@ main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  List<String> _products = ["New List"];
+
+  @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          centerTitle: true,
-          title: Text("My App"),
-        ),
-        body: Card(
-          child: Column(
-            children: [Image.asset("assets/952228.jpg"), Text("Goku/Jiren")],
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            centerTitle: true,
+            title: Text("My App"),
           ),
-        ),
-      ),
+          body: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                child: RaisedButton(
+                  onPressed: () => {},
+                  color: Colors.pinkAccent,
+                  textColor: Colors.black,
+                  child: Text("Add"),
+                ),
+              ),
+              Column(
+                children: _products
+                    .map((e) => Card(
+                          child: Column(
+                            children: [
+                              Image.asset("assets/952228.jpg"),
+                              Text(e)
+                            ],
+                          ),
+                        ))
+                    .toList(),
+              )
+            ],
+          )),
     );
   }
 }
